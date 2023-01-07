5e6704279bd616c3995771ab66cc57cb5ab038a2
vedjangid19 <vedprakashjangid.jpr@gmail.com>
2023-01-07 14:09:36 +0530

    Complete python Basic Assignments.

---
+++ python baasic assinement theory/multi_process.py
@@ -0,0 +1,12 @@
+import multiprocessing
+def now(seconds):
+        from datetime import datetime
+        from time import sleep
+        sleep(seconds)
+        print('wait', seconds, 'seconds, time is', datetime.utcnow())
+if __name__ == '__main__':
+        import random
+        for n in range(3):
+                seconds = random.random()
+                proc = multiprocessing.Process(target=now, args=(seconds,))
+                proc.start()
